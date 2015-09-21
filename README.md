10d487865dfab2ba84528176e82a4f40861aea75
Jamund Ferguson <jamuferguson@paypal.com>
2015-09-21 10:50:04 -0700

    updated readme

--- README.md
+++ README.md
@@ -8,3 +8,22 @@ PDF: [presentation.pdf](https://github.com/xjamundx/asts-utahjs-2015/blob/master
 Code: [compare.js](https://github.com/xjamundx/asts-utahjs-2015/blob/master/code/compare.js)
 
 Presented with [Deckset](http://www.decksetapp.com)
+
+
+## Example
+
+
+To run the compare script:
+
+```
+cp code/complex2.js code/complex1.js
+git diff --raw code/complex1.js | node code/compare.js
+```
+
+Results:
+
+```
+~/asts-utahjs-2015/code/complex1.js
+1. The exported `buildHouse` function went from a return to a callback.
+2. The private `getPermits` function was added.
+```
--- code/complex2.js
+++ code/complex2.js
@@ -5,13 +5,13 @@ function getPermits(callback) {
 export function buildHouse(lot, color, size, bedrooms, callback) {
 	getPermits((permits) => {
 		clearLot(permits, lot);
-	let foundation = buildFoundation(size);
-	let walls = buildWalls(bedrooms);
-	let paintedWalls = paintWalls(color, walls);
-	let roof = buildRoof(foundation, walls);
-	let house = foundation + paintedWalls + roof;
+		let foundation = buildFoundation(size);
+		let walls = buildWalls(bedrooms);
+		let paintedWalls = paintWalls(color, walls);
+		let roof = buildRoof(foundation, walls);
+		let house = foundation + paintedWalls + roof;
 
-	// house will be ready in a year
-	callback(house);
-});
+		// house will be ready in a year
+		callback(house);
+	});
 }
